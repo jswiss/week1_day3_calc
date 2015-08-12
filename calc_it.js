bed00feffee9e8d0c9bc496ed1ab779ddec7c781
Joshua Swiss <swissjoshua@gmail.com>
2015-08-12 19:04:23 +0100

    Added sqrt and exponentials

--- calc_it.js
+++ calc_it.js
@@ -3,28 +3,33 @@ alert("Please make sure your console is open");
 var num = parseInt(prompt('choose a number'));
 console.log('num', num);
 
-var op = prompt('choose an operation (i.e. +, -, *, /)');
+var op = prompt('choose an operation (i.e. + = add, - = subtract, * = multiply, / = divide, sqrt = square root, exp = exponent)');
 console.log('op', op);
 
-var numDos = parseInt(prompt('choose a second number'));
-console.log('numDos', numDos);
-
 if (isNaN(num) === true && isNaN(numDos) === true) {
-  alert('Try entering a real number ya idjit.');
+  alert('Try entering a real number ya idjit. Hit refresh to try again');
+  } else if(op !== "sqrt") {
+    var numDos = parseInt(prompt('choose a second number'));
+    console.log('numDos', numDos);
   } else if(op === "+") {
-  prompt(num + numDos);
+  alert(num + numDos);
   console.log(num + numDos);
 }  else if (op === "-") {
-  prompt(num - numDos);
+  alert(num - numDos);
   console.log(num - numDos);
 }  else if (op === "*") {
-  prompt(num * numDos);
+  alert(num * numDos);
   console.log(num * numDos);
 }  else if (op === "/") {
-  prompt(num / numDos);
+  alert(num / numDos);
   console.log(num / numDos);
+}   else if (op === "sqrt") {
+  alert(Math.sqrt(num));
+  console.log(Math.sqrt(num));
+}   else if (op === "exp") {
+  alert(Math.pow(num, numDos));
+  console.log(Math.pow(num, numDos));
   } else {
-    prompt(num + numDos);
     console.log("Error")
   };
 
